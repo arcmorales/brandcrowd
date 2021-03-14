0110f1adef4d84f90ef6cf248e96a366aeb45bcd
Ara <arcmorales86@gmail.com>
2021-03-15 00:49:03 +1100

    chore: refactor tests and update README

--- README.md
+++ README.md
@@ -5,3 +5,22 @@
 3. To launch the test runner, run `npm run cypress:open`. The test runner is useful as it allows you to view the application and see commands as they execute.
 4. Click on `logoSearch.spec.js` to run the test.
 5. Alternatively, to run the test in headless mode, run `npm run cypress:run`
+
+---
+## Concepts
+
+### Fixtures
+
+- Use of fixtures to define data sets
+- Use of viewports to test against different screensizes (responsive view). This example uses 1920x1080
+
+### Selectors
+- Use of `data-test*`  attributes if applicable. Other selectors are frequently subjected to change and may be volatile.
+
+### Grouping Tests
+- Avoid dependence on the previous' test's state. If only one test is to run, example via `it.only`, it should still execute because it does not have a dependency to the other tests' state.
+- Adding multiple assertions if applicable to make tests performant
+- Avoid writing small assertions per (it)
+
+### Using cypress config
+- Configuring baseUrl in cypress config
--- cypress/integration/tests/logoSearch.spec.js
+++ cypress/integration/tests/logoSearch.spec.js
@@ -15,21 +15,25 @@ describe('BrandCrowd Logo search form', () => {
     })
 
     it('should display logo filter options', () => {
-        cy.get('button[data-toggle="filter-bar__more filter-bar__more-button filter-bar__less-button"]').click()
-        cy.get('select#LogoStyle').should('be.visible')
-        cy.get('#font-styles-dropdown').should('be.visible')
+        cy.get('[data-toggle="filter-bar__more filter-bar__more-button filter-bar__less-button"]').click()
+        cy.get('[data-test-business-name-search-results]').should('be.visible')
+        cy.get('[data-test-keyword-search]').should('be.visible')
+        cy.get('[data-test-logo-style-search]').should('be.visible')
+        cy.get('[data-test-font-style-filter]').should('be.visible')
         cy.get('#colors-dropdown').should('be.visible')
+        cy.get('[data-toggle="filter-bar__more filter-bar__more-button filter-bar__less-button"]').click()
     });
 
-    it('should search for logo based on filter', () => {
-        cy.get('#search-form input#gtm-SearchText[type="text"]').type(searchTerms.business)
-        cy.get('#search-form input#SearchText[type="text"]').type(searchTerms.keyword)
-        cy.get('select#LogoStyle').select(searchTerms.style)
-        cy.get('#font-styles-dropdown').click()
-        cy.get('#font-styles-dropdown input[type="checkbox"]').check(searchTerms.font, { scrollBehavior: 'center', force: true })
-        cy.get('#colors-dropdown input[type="checkbox"]').check('blue', { scrollBehavior: 'center', force: true })
-        cy.get('#search-form button[data-test-create-logos-btn]').click()
-        cy.get('.card-design img[data-test-logo-design-images]').should('have.length.at.least', 1)
+    it('should search for logos based on filter', () => {
+        cy.get('[data-toggle="filter-bar__more filter-bar__more-button filter-bar__less-button"]').click()
+        cy.get('[data-test-business-name-search-results]').type(searchTerms.business)
+        cy.get('[data-test-keyword-search]').type(searchTerms.keyword)
+        cy.get('[data-test-logo-style-search]').select(searchTerms.style)
+        cy.get('[data-test-font-style-filter]').click()
+        cy.get('[data-test-font-style-filter] input[type="checkbox"]').check(searchTerms.font, { scrollBehavior: 'center', force: true })
+        cy.get('#colors-dropdown input[type="checkbox"]').check(searchTerms.color, { scrollBehavior: 'center', force: true })
+        cy.get('[data-test-create-logos-btn]').click()
+        cy.get('[data-test-logo-design-images]').should('have.length.at.least', 1)
             .each($searchResult => {
                 expect($searchResult).to.have.attr('src').to.contain(searchTerms.business)
             })
